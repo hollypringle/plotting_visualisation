72f924c6fbe0ce2c95f67e73075d36e2ace09b31
Holly Pringle <57662494+hollypringle@users.noreply.github.com>
2022-04-24 12:35:17 +0100

    Update Pressure_Maps_Leaflet.R

--- Pressure_Maps_Leaflet.R
+++ Pressure_Maps_Leaflet.R
@@ -41,49 +41,6 @@ viz_map_shoat <- covs_livestockrates %>%
                    width = 250,
                    height = 40,)
 
-qpal <- colorQuantile("OrRd", covs_livestockrates$cattle_5min_event_rate, n = 5)
-
-
-
-# !formatR
-library(leaflet)
-
-# an automatic legend derived from the color palette
-pal <- colorNumeric("OrRd", df$z)
-leaflet(df) %>%
-  addTiles() %>%
-  addCircleMarkers(~x, ~y, color = ~pal(z), group = "circles") %>%
-  addLegend(pal = pal, values = ~z, group = "circles", position = "bottomleft") %>%
-  addLayersControl(overlayGroups = c("circles"))
-
-# format legend labels
-df <- data.frame(x = rnorm(100), y = rexp(100, 2), z = runif(100))
-pal <- colorBin("PuOr", df$z, bins = c(0, .1, .4, .9, 1))
-leaflet(df) %>%
-  addTiles() %>%
-  addCircleMarkers(~x, ~y, color = ~pal(z), group = "circles") %>%
-  addLegend(pal = pal, values = ~z, group = "circles", position = "bottomleft") %>%
-  addLayersControl(overlayGroups = c("circles"))
-
-leaflet(df) %>%
-  addTiles() %>%
-  addCircleMarkers(~x, ~y, color = ~pal(z), group = "circles") %>%
-  addLegend(pal = pal, values = ~z, labFormat = labelFormat(
-    prefix = "(", suffix = ")%", between = ", ",
-    transform = function(x) 100 * x
-  ),  group = "circles", position = "bottomleft" ) %>%
-  addLayersControl(overlayGroups = c("circles"))
-
-
-# creating a heat map for shoat
-viz_map_shoat <- covs_livestockrates %>%
-  leaflet() %>% 
-  addTiles() %>% 
-  addProviderTiles(providers$OpenStreetMap) %>% 
-  setView(long_center,lat_center,6) %>%
-  addHeatmap(lng=~longitude,lat=~latitude,intensity=~shoat_5min_event_rate,max=100,radius=25,blur=20)
-
-
 # plot into a 1x2 grid; for that use the "mapview" package in R
 install.packages("mapview")
 library(mapview)
